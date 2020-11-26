05f96a66fe21cbedfd2651e371052581592e5566
iamrealnurs <nurs567@yandex.ru>
2020-11-26 17:57:26 +0600

    Added the path for the static files in settings.py Added lesson_3 to apps list in settings.py Added picture to lesson 3 statics files folder

--- django_starter/settings.py
+++ django_starter/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lesson_3',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'lesson_3/static/'
